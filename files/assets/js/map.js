2f5602850f8e124e0acdb42287b1a0bc41f379f8
PN <pn@klapproth-koch.de>
2022-06-29 10:48:30 +0200

    Vor der Scroll

---
+++ files/assets/js/map.js
@@ -0,0 +1,19 @@
+const markerIcon = L.icon({
+    iconUrl: 'files/layout/2016/mobil_google_maps.png',
+    iconSize: [50, 50],
+    iconAnchor: [18, 60]
+});
+
+const map = L.map('map', {
+    scrollWheelZoom: false
+}).setView([50.987222,11.326138], 16);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/floriantepelmann/cl4xw7a3c004u14q8n4sgphfr/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 22,
+    accessToken: 'pk.eyJ1IjoiZmxvcmlhbnRlcGVsbWFubiIsImEiOiJjbDR4emFzbW4xbjBuM2VwaG15c3NhbXcyIn0.1YvY1-lUe64NOobEZKhNVQ'
+}).addTo(map);
+
+let coords = [50.987222,11.326138];
+let marker = L.marker(coords, {icon: markerIcon}).addTo(map);
+marker.bindPopup('<strong>Hier findest du uns:</strong><br>Carl-August-Allee 1<br>99423 Weimar / Thüringen');
