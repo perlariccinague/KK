1372b82d7762acea6456e1489ddf7f176d465968
Florian Tepelmann <ft@klapproth-koch.de>
2022-07-21 17:03:49 +0200

    Go-Live

--- src/scrollEffect.js
+++ src/scrollEffect.js
@@ -1,9 +1,12 @@
 import Lenis from '@studio-freight/lenis'
-/*import { gsap } from 'gsap';
+import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);*/
+gsap.registerPlugin(ScrollTrigger);
 
-// const paths = [...document.querySelectorAll('path.path-anim')];
+const pathsTop = [...document.querySelectorAll('path.path-anim-top')];
+const pathsBottom = [...document.querySelectorAll('path.path-anim-bottom')];
+const shadows = document.querySelector('.bottom-shadow-boxes');
+const topShadow = document.querySelector('.bottom-shadow-box-top');
 
 const lenis = new Lenis({
     lerp: 0.1,
@@ -15,14 +18,14 @@ const scrollFn = () => {
 };
 requestAnimationFrame(scrollFn);
 
-/*paths.forEach(el => {
+pathsTop.forEach(el => {
     const svgEl = el.closest('svg');
     const pathTo = el.dataset.pathTo;
 
     gsap.timeline({
         scrollTrigger: {
             trigger: svgEl,
-            start: "top bottom",
+            start: "top top",
             end: "bottom top",
             scrub: true
         }
@@ -31,4 +34,49 @@ requestAnimationFrame(scrollFn);
             ease: 'none',
             attr: { d: pathTo }
         });
-});*/
+});
+
+pathsBottom.forEach(el => {
+    const svgEl = el.closest('svg');
+    const pathTo = el.dataset.pathTo;
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: svgEl,
+            start: 0,
+            end: "top top",
+            scrub: true
+        }
+    })
+        .to(el, {
+            ease: 'none',
+            attr: { d: pathTo }
+        });
+});
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: shadows,
+        start: 0,
+        end: "top top",
+        scrub: true
+    }
+})
+    .to(shadows, {
+        ease: 'none',
+        top: '60px',
+        width: '1160px'
+    })
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: topShadow,
+        start: 0,
+        end: "top top",
+        scrub: true
+    }
+})
+    .to(topShadow, {
+        ease: 'none',
+        top: '-5px'
+    })
--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <div class="header_contain">
     {{insert_module::85}}
     {{insert_module::97}}
-    {{insert_module::79}}
+    {{insert_module::79}} <!-- Header: Customnav -->
     {{insert_content::3973}}
     {{insert_content::3974}}
     {{insert_content::4024}}
